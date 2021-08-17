615e02d91efabf502ac4112cc3dfa0eb6cefdd09
Megha <meghajagadale99@gmail.com>
2021-08-17 21:29:11 +0530

    Added: find maximum between three integer

--- src/main/java/com/bridgelabz/FindMax.java
+++ src/main/java/com/bridgelabz/FindMax.java
@@ -2,7 +2,18 @@ package com.bridgelabz;
 
 public class FindMax {
 
+    public static Integer maxOfInteger(Integer a1, Integer a2, Integer a3) {
+        Integer max = a1;
+        if (a2.compareTo(max) > 0)
+            max = a2;
+        if (a3.compareTo(max) > 0)
+            max = a3;
+        return max;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome To Test Maximum Programs");
+        System.out.println("Welcome to the program to find maximum value");
+        Integer a1 = 30, a2 = 98, a3 = 28;
+        System.out.println("The Maximum between three integers is : " + maxOfInteger(a1, a2, a3));
     }
 }
