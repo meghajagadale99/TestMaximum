20c1c43a461d704ceff886455b12e2d28e244213
Megha <meghajagadale99@gmail.com>
2021-08-18 10:15:34 +0530

    Added: find maximum between three String

--- src/main/java/com/bridgelabz/FindMax.java
+++ src/main/java/com/bridgelabz/FindMax.java
@@ -20,11 +20,22 @@ public class FindMax {
         return max;
     }
 
+    public static String maxOfString(String s1, String s2, String s3) {
+        String max = s1;
+        if (s2.compareTo(max) > 0)
+            max = s2;
+        if (s3.compareTo(max) > 0)
+            max = s3;
+        return max;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the program to find maximum value");
         Integer a1 = 30, a2 = 98, a3 = 28;
         System.out.println("The Maximum between three integers is : " + maxOfInteger(a1, a2, a3));
         Float f1 = 4.7f, f2 =12.4f, f3 = 53.2f;
         System.out.println("The Maximum between three float is : " + maxOfFloat(f1, f2, f3));
+        String s1 = "megha", s2 = "neha", s3 = "rupa";
+        System.out.println("The Maximum between three String is : " + maxOfString(s1, s2, s3));
     }
 }
