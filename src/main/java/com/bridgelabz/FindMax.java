0802151fe1317afd483729b6ae8cdf2c332b17df
Megha <meghajagadale99@gmail.com>
2021-08-18 07:57:53 +0530

    Added: find maximum between three values

--- src/main/java/com/bridgelabz/FindMax.java
+++ src/main/java/com/bridgelabz/FindMax.java
@@ -11,9 +11,20 @@ public class FindMax {
         return max;
     }
 
+    public static Float maxOfFloat(Float f1, Float f2, Float f3) {
+        Float max = f1;
+        if (f2.compareTo(max) > 0)
+            max = f2;
+        if (f3.compareTo(max) > 0)
+            max = f3;
+        return max;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the program to find maximum value");
         Integer a1 = 30, a2 = 98, a3 = 28;
         System.out.println("The Maximum between three integers is : " + maxOfInteger(a1, a2, a3));
+        Float f1 = 4.7f, f2 =12.4f, f3 = 53.2f;
+        System.out.println("The Maximum between three float is : " + maxOfFloat(f1, f2, f3));
     }
 }
