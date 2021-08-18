da67982091ee969a2dab2a9cee277f291d432dec
Megha <meghajagadale99@gmail.com>
2021-08-18 16:22:57 +0530

    Added:print the max to std out using generic method

--- src/main/java/com/bridgelabz/FindMax.java
+++ src/main/java/com/bridgelabz/FindMax.java
@@ -13,15 +13,16 @@ public class FindMax<T extends Comparable<T>> {
         Arrays.sort(elements);
         int length = elements.length;
         T max = elements[length - 1];
+        System.out.printf("Maximum value of three is %s :  ", max);
         return max;
     }
     public static void main(String[] args) {
         System.out.println("Welcome to the program to find maximum value using generics");
         Integer[] intMax ={ 30, 98, 28, 23 ,45,};
-        System.out.println("The Maximum between three integers is : " + maxOfValues(intMax));
+        maxOfValues(intMax);
         Float[] floatMax = {4.7f, 12.4f, 53.2f, 3.67f, 23.5f};
-        System.out.println("The Maximum between three float is : " + maxOfValues(floatMax));
+        maxOfValues(floatMax);
         String[] stringMax = {"megha", "neha", "pranali", "alka", "prakash"};
-        System.out.println("The Maximum between three String is : " +maxOfValues(stringMax));
+        maxOfValues(stringMax);
     }
 }
