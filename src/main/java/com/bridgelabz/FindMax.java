45970a1593c7b3272d4f8dd5fd68484dbaa5edd0
Megha <meghajagadale99@gmail.com>
2021-08-18 10:56:20 +0530

    Added: using generic method to find maximum

--- src/main/java/com/bridgelabz/FindMax.java
+++ src/main/java/com/bridgelabz/FindMax.java
@@ -1,9 +1,8 @@
 package com.bridgelabz;
 
 public class FindMax {
-
-    public static Integer maxOfInteger(Integer a1, Integer a2, Integer a3) {
-        Integer max = a1;
+    public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
+        T max = a1;
         if (a2.compareTo(max) > 0)
             max = a2;
         if (a3.compareTo(max) > 0)
@@ -11,31 +10,13 @@ public class FindMax {
         return max;
     }
 
-    public static Float maxOfFloat(Float f1, Float f2, Float f3) {
-        Float max = f1;
-        if (f2.compareTo(max) > 0)
-            max = f2;
-        if (f3.compareTo(max) > 0)
-            max = f3;
-        return max;
-    }
-
-    public static String maxOfString(String s1, String s2, String s3) {
-        String max = s1;
-        if (s2.compareTo(max) > 0)
-            max = s2;
-        if (s3.compareTo(max) > 0)
-            max = s3;
-        return max;
-    }
-
     public static void main(String[] args) {
         System.out.println("Welcome to the program to find maximum value");
         Integer a1 = 30, a2 = 98, a3 = 28;
-        System.out.println("The Maximum between three integers is : " + maxOfInteger(a1, a2, a3));
+        System.out.println("The Maximum between three integers is : " + maxOfValues(a1, a2, a3));
         Float f1 = 4.7f, f2 =12.4f, f3 = 53.2f;
-        System.out.println("The Maximum between three float is : " + maxOfFloat(f1, f2, f3));
+        System.out.println("The Maximum between three float is : " + maxOfValues(f1, f2, f3));
         String s1 = "megha", s2 = "neha", s3 = "rupa";
-        System.out.println("The Maximum between three String is : " + maxOfString(s1, s2, s3));
+        System.out.println("The Maximum between three String is : " +maxOfValues(s1, s2, s3));
     }
 }
